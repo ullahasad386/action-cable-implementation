c1d463d103f81595de44d615258ed26c613a6318
ullahasad386 <ullahasad386@gmail.com>
2019-08-05 11:38:58 +0500

    Completed one to one convo

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
+
+  validates :body, presence: true
+
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
 end
